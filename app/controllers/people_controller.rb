99f4326b70663d44fef3a96272d6641ba2dfc57c
Harsha Goli <harshagoli@gmail.com>
2017-11-06 09:47:51 -0500

    Implemented api for specific events

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,9 @@ class PeopleController < ApplicationController
   def for_event
   	@people = Event.find(params[:events_id]).people 
   end
+
+  def for_eventjson
+  	people = Event.find(params[:for_event]).people
+  	render json: {status: 'SUCCESS', message: 'Loaded all events', data: people}, status: :ok  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   get 'events/attendees', to: 'people#for_event'
 
-  post 'people/for_event', to: 'people#for_event'
-
   get 'events/', to: 'events#index'
 
   #Mark REST endpoints
 
   get 'events/all', to: 'events#all'
+
+  get 'people/:for_event', to: 'people#for_eventjson'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
