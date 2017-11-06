d9a8abec10a2cd8f3a0fa5ad9a3e8ec98ec31345
Harsha Goli <harshagoli@gmail.com>
2017-11-06 09:39:57 -0500

    Removed clutter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,23 +12,4 @@ class EventsController < ApplicationController
   	render json: {status: 'SUCCESS', message: 'Loaded all events', data: events}, status: :ok 
     
   end
-
-#   def create
-# =begin
-#   	@event = Event.new(params[:event])
-# 	@organizers = Organizer.where(:id => params[:organizing_team])
-# 	@event.organizers << @organizers 
-# =end
-# 	#associate the selected organizers to the event and create records in the join table
-#   end 
-
-#   def update
-#   	def update
-# 	@event = Event.find(params[:id])
-# 	@person = Organizer.where(:id => params[:organizing_team])
-# 	@event.organizers.destroy_all   #disassociate the already added organizers
-# 	@event.organizers << @organizers 
-# 	#associate the selected organizers to the event and create records in the join table
-#   end
-
 end
